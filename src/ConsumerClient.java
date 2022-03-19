914ca12b63a03d7059eada24bc702919e4b4849e
sanshididi <2248511201@qq.com>
2022-03-19 14:35:26 +0800

    first commit

---
+++ src/ConsumerClient.java
@@ -0,0 +1,7 @@
+public class ConsumerClient {
+    public static void main(String[] args) throws Exception{
+        MQClient client = new MQClient();
+        String message = client.consume();
+        System.out.println("获取的消息为： "+message);
+    }
+}
